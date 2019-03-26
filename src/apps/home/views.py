d0065233eed30120123873322faf57156a090c6d
Nicolas Vargas <ndvargas95@gmail.com>
2019-03-25 22:49:23 -0500

    Home page added

--- src/apps/home/views.py
+++ src/apps/home/views.py
@@ -6,10 +6,6 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def index(request):
-    """ class for homepage """
+    """ function for homepage """
     # print(request.user.r_profile.property_relation)
-    if request.user.role == 1:
-        msg = ' y eres un residente'
-    elif request.user.role == 2:
-        msg = ' y eres un vigilante'
-    return HttpResponse('Welcome to the homepage {0}, {1}'.format(request.user.username, msg))
+    return render(request, 'home/home.html')
